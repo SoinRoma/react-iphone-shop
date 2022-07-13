f488e3c4732ff315f5442a71dc4a7c56f2163f88
SoinRoma <soinroma26@gmail.com>
2022-07-13 12:42:31 +0500

    - add base structure and style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import './assets/css/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="wrapper">
+        <header className="d-flex">
+           <div className="header-left">
+               <img src="./images/logo.png" alt="logo" width="40" height="40"/>
+               <div className="header-info">
+                   <h3>React Sneakers</h3>
+                   <p>Магазин лучших кроссовок</p>
+               </div>
+           </div>
+            <ul className="header-right">
+                <li>
+                    <svg></svg>
+                    <span>1205 руб.</span>
+                </li>
+                <li>
+                    <svg></svg>
+                </li>
+            </ul>
+        </header>
+        <div className="content">
+            <h1>Все кроссовки</h1>
+            <div className="cards">
+                ...
+            </div>
+        </div>
     </div>
   );
 }
