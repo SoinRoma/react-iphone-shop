43174f8a8d463e9cf619346dbdd50773a00912f3
SoinRoma <soinroma26@gmail.com>
2022-07-13 16:35:03 +0500

    - add buttons for card - add hover for card

--- src/App.js
+++ src/App.js
@@ -33,9 +33,20 @@ function App() {
             </ul>
         </header>
         <section className="content">
-            <h1>Все кроссовки</h1>
+            <div className="d-flex justify-between align-center">
+                <h1>Все кроссовки</h1>
+                <div className="search-block d-flex align-center">
+                    <label htmlFor="search-input" className="d-flex align-center cu-p">
+                        <img src="./svg/search.svg" alt="search"/>
+                    </label>
+                    <input type="text" placeholder="Поиск..." id="search-input"/>
+                </div>
+            </div>
             <div className="d-flex flex-wrap align-center cards">
                 <div className="card">
+                    <div className="favourite">
+                        <img src="./svg/heard-unlike.svg" alt="unlike"/>
+                    </div>
                     <img src="./images/sneakers/1.png" alt="sneaker1" width="133" height="112"/>
                     <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
                     <div className="d-flex justify-between align-center">
@@ -44,20 +55,7 @@ function App() {
                             <span>12 999 руб.</span>
                         </div>
                         <button className="cu-p">
-                            <img src="./svg/plus.svg" alt="sneaker1" width="11" height="11"/>
-                        </button>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="./images/sneakers/2.png" alt="sneaker1" width="133" height="112"/>
-                    <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
-                    <div className="d-flex justify-between align-center">
-                        <div className="card-price">
-                            <p>Цена:</p>
-                            <span>12 999 руб.</span>
-                        </div>
-                        <button className="cu-p">
-                            <img src="./svg/plus.svg" alt="sneaker1" width="11" height="11"/>
+                            <img src="./svg/button-uncheck.svg" alt="uncheck" width="32" height="32"/>
                         </button>
                     </div>
                 </div>
