c30c248550cc9a02a60b8f05e21cdc268a76ca2d
SoinRoma <soinroma26@gmail.com>
2023-11-23 12:26:26 +0500

    - added check if cart is empty (style)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -16,40 +16,52 @@ const Cart = ({cartItems = [], setCartItems, setIsCartOpen}) => {
             <img src="./svg/close.svg" alt="close"/>
           </button>
         </div>
-        <div className="items">
-          {cartItems.length ?
-            cartItems.map(item =>
-              <div className="cart-item d-flex justify-between align-center" key={item.id}>
-                <img src={item.imageUrl} alt="sneakers" width="70"/>
-                <div className="cart-text">
-                  <p> {item.name}</p>
-                  <span>{item.price} руб.</span>
-                </div>
-                <button className="cu-p" onClick={() => deleteItem(item.id)}>
-                  <img src="./svg/close.svg" alt="close"/>
+        {cartItems.length ? (
+            <div>
+              <div className="items">
+                {cartItems.map(item =>
+                  <div className="cart-item d-flex justify-between align-center" key={item.id}>
+                    <img src={item.imageUrl} alt="sneakers" width="70"/>
+                    <div className="cart-text">
+                      <p> {item.name}</p>
+                      <span>{item.price} руб.</span>
+                    </div>
+                    <button className="cu-p" onClick={() => deleteItem(item.id)}>
+                      <img src="./svg/close.svg" alt="close"/>
+                    </button>
+                  </div>)}
+              </div>
+              <div className="drawer-bottom">
+                <ul>
+                  <li className="d-flex justify-between align-end">
+                    <p>Итого:</p>
+                    <div/>
+                    <b>21 498 руб.</b>
+                  </li>
+                  <li className="d-flex justify-between align-end">
+                    <p>Налог 5%:</p>
+                    <div/>
+                    <b>1074 руб.</b>
+                  </li>
+                </ul>
+                <button>
+                  <span>Оформить заказ</span>
+                  <img src="./svg/arrow.svg" alt="arrow"/>
                 </button>
               </div>
-            )
-            : <h3>Корзина пустая!</h3>}
-        </div>
-        <div className="drawer-bottom">
-          <ul>
-            <li className="d-flex justify-between align-end">
-              <p>Итого:</p>
-              <div/>
-              <b>21 498 руб.</b>
-            </li>
-            <li className="d-flex justify-between align-end">
-              <p>Налог 5%:</p>
-              <div/>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
-          <button>
-            <span>Оформить заказ</span>
-            <img src="./svg/arrow.svg" alt="arrow"/>
-          </button>
-        </div>
+            </div>
+          )
+          :
+          <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+            <img className="mb-20" width="120px" src="./images/empty-cart.jpg" alt="Empty"/>
+            <h2>Корзина пустая</h2>
+            <p className="opacity-6">Добавьте хотя бы один товар!</p>
+            <button className="greenButton" onClick={() => setIsCartOpen(false)}>
+              <img src="./svg/arrow.svg" alt="Arrow"/>
+              Вернуться назад
+            </button>
+          </div>
+        }
       </div>
     </div>
   );
