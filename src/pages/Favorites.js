9f550bd8071e69aa79fb4a63b32c65f6d26c24de
SoinRoma <soinroma26@gmail.com>
2023-11-24 10:44:33 +0500

    - added style for favorites (empty)

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,14 +1,20 @@
 import Card from "../components/Card/Card"
+import {Link} from "react-router-dom";
+import React from "react";
 
 function Favorites({favorites, isItemAdded, isItemFavorite, addCartItem, toggleFavorite}) {
   return (
     <section className="content">
-      <div className="d-flex justify-between align-center">
+      <div className="d-flex align-center">
+        <Link to="/" className="btn-back">
+          <img src="./svg/btn-back.svg" alt="back"/>
+        </Link>
         <h1>Мои закладки</h1>
       </div>
-      <div className="d-flex flex-wrap align-center cards">
+      <div>
         {favorites.length ?
-          favorites.map((item) =>
+          <div className="d-flex flex-wrap align-center cards">
+            {favorites.map((item) =>
             <Card
               key={item.id}
               item={item}
@@ -16,9 +22,18 @@ function Favorites({favorites, isItemAdded, isItemFavorite, addCartItem, toggleF
               isItemFavorite={isItemFavorite}
               toggleFavorite={toggleFavorite}
               addCartItem={addCartItem}/>
-          )
+            )}
+          </div>
           :
-          <h5>Список пуст!</h5>
+          <div className="d-flex flex-column justify-center align-center mt-50">
+            <img className="mb-20" width="120px" src="./images/emoji.png" alt="emoji"/>
+            <h3 className="empty-title">Список пуст!</h3>
+            <p className="empty-subtitle">Вы ничего не добавляли в закладки</p>
+            <Link to="/" className="greenButton">
+              <img src="./svg/arrow.svg" alt="Arrow"/>
+              Вернуться назад
+            </Link>
+          </div>
         }
       </div>
     </section>
